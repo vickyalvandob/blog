2c589e47e70647f18beeee2ea1e03fe712046fd7
vickyalvandob <73622790+vickyalvandob@users.noreply.github.com>
2025-05-13 11:01:55 +0700

    update

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+      use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'category_id',
+        'published_at'
+    ];
+
+    public function category():BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function comments():HasMany
+    {
+        return $this->belongsTo(Comment::class);
+    }
+}
